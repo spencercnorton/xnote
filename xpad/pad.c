47873de9fd8074a5f565ca3ef63e296eeda7e0e7
mikix <>
2002-07-22 22:07:02 +0000

    documentation for edit lock is now in

--- xpad/pad.c
+++ xpad/pad.c
@@ -262,6 +262,7 @@ void about_dialog (pad_node *pad)
 void help_dialog (void)
 {
 	GtkWidget *dialog, *helptext, *helplabel, *button, *notebook, *keytext, *keylabel;
+	GtkWidget *edittext, *editlabel;
 	
 	/* Create the widgets */
 	
@@ -270,6 +271,8 @@ void help_dialog (void)
 	helplabel = gtk_label_new ("Introduction");
 	keytext = gtk_label_new ("");
 	keylabel = gtk_label_new ("Keyboard Shortcuts");
+	edittext = gtk_label_new ("");
+	editlabel = gtk_label_new ("Edit Lock");
 	button = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
 	notebook = gtk_notebook_new ();
 	
@@ -321,6 +324,7 @@ void help_dialog (void)
 	gtk_label_set_line_wrap (GTK_LABEL (helptext), TRUE);
 	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), helptext, helplabel);
 	
+	
 	gtk_label_set_markup (GTK_LABEL (keytext),
 "<b>CTRL+n</b>: Creates a new pad.\n\n"
 "<b>CTRL+s</b>: Saves the contents of a pad to a file.\n\n"
@@ -333,6 +337,24 @@ void help_dialog (void)
 	
 	gtk_label_set_line_wrap (GTK_LABEL (keytext), TRUE);
 	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), keytext, keylabel);
+	
+	
+	gtk_label_set_markup (GTK_LABEL (edittext),
+"If Edit Lock is enabled, a pad is always in one of two "
+"modes:  Edit Mode or Move Mode.\n\n"
+"<b>Edit Mode</b>: You can edit the text of the pad, select "
+"text, cut and paste, etc.\n\n"
+"<b>Move Mode</b>: In this mode, clicking and dragging on the "
+"pad will move the pad, rather than selecting text.  You cannot "
+"edit the contents of the pad.\n\n"
+"Any pad without focus is in Move Mode.  To enter Edit "
+"Mode for any pad, double click on it with the left mouse "
+"button.  Now this pad will be editable until it loses focus.\n\n"
+"If Edit Lock is disabled, all pads are always in Edit Mode.\n");
+
+	gtk_label_set_line_wrap (GTK_LABEL (edittext), TRUE);
+	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), edittext, editlabel);
+	
 	
 	gtk_window_set_title (GTK_WINDOW (dialog), "xpad help");
 	
