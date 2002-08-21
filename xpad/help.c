f192f88eda7f0acee74a2cb92579b39b749b5783
mikix <>
2002-08-21 03:00:06 +0000

    change the main help window a bit to reflect more information being in the preference window and toolbar changes

--- xpad/help.c
+++ xpad/help.c
@@ -34,32 +34,11 @@ static void show_help_at_page (gint page);
 static GtkWidget *create_help (gint page)
 {
 	GtkWidget *dialog, *helptext, *helplabel, *button, *notebook, *keytext, *keylabel;
-	GtkWidget *edittext, *editlabel;
-	GtkWidget *align1, *align2, *align3;
+	GtkWidget *styletext, *stylelabel;
 	
 	/**
-	 * Ok.  This goofy construction is because of the gcc warning:
-	 * "string length `658' is greater than the length `509' ISO C89 compilers
-	 * are required to support" -- so, we split it up into two smaller chunks for those
-	 * crazy old compilers.
+	 * NOTE:  ISO C89 compilers don't have to support strings larger than `509'.
 	 */
-	gchar longtext [700];
-	
-	strcpy (longtext, 
-"If Edit Lock is enabled, a pad is always in one of two "
-"modes:  Edit Mode or Move Mode.\n\n"
-"<b>Edit Mode</b>: You can edit the text of the pad, select "
-"text, cut and paste, etc.  To move the pad, hold down CTRL "
-"while left-dragging.\n\n"
-"<b>Move Mode</b>: In this mode, clicking and dragging on the ");
-	strcat (longtext,
-"pad will move the pad, rather than selecting text.  You cannot "
-"edit the contents of the pad.\n\n"
-"Any pad without focus is in Move Mode.  To enter Edit "
-"Mode for any pad, double click on it with the left mouse "
-"button.  Now this pad will be editable until it loses focus.\n\n"
-"If Edit Lock is disabled, all pads are in Edit Mode.\n\n"
-"To enable Edit Lock, go to Preferences->Options.");
 	
 	/* Create the widgets */
 	
@@ -68,18 +47,22 @@ static GtkWidget *create_help (gint page)
 	helplabel = gtk_label_new ("Introduction");
 	keytext = gtk_label_new ("");
 	keylabel = gtk_label_new ("Keyboard Shortcuts");
-	edittext = gtk_label_new ("");
-	editlabel = gtk_label_new ("Edit Lock");
+	styletext = gtk_label_new ("");
+	stylelabel = gtk_label_new ("Style Locking");
 	notebook = gtk_notebook_new ();
-	align1 = gtk_alignment_new (0, 0, 0, 0);
-	align2 = gtk_alignment_new (0, 0, 0, 0);
-	align3 = gtk_alignment_new (0, 0, 0, 0);
 	
 	gtk_label_set_markup (GTK_LABEL (helptext), 
 "Each xpad session consists of one or more open pads.  "
 "These pads are basically textboxes on your desktop in which "
 "you can write memos.\n\n"
 
+"The contents of pads are transparently "
+"saved and reloaded when xpad is next started.\n\n"
+
+"To move a pad, left drag on the toolbar or right drag "
+"on the resizer in the bottom right.  To resize a pad, "
+"left drag on the resizer.\n\n"
+/*
 "There are two important non-obvious operations that you "
 "should be aware of:\n\n"
 
@@ -88,12 +71,19 @@ static GtkWidget *create_help (gint page)
 
 "<b>Resizing</b>: To resize a pad, hold down CTRL and "
 "drag with the right mouse button.\n\n"
+*/
+"Most actions are available throught the popup menu "
+"that appears when you right click on a pad.  Try it out and "
+"enjoy.\n\n"
+
+"Please send comments or bug reports to "
+"xpad-devel@lists.sourceforge.net"
 );
 
+	gtk_misc_set_padding (GTK_MISC (helptext), 12, 12);
+	gtk_misc_set_alignment (GTK_MISC (helptext), 0, 0);
 	gtk_label_set_line_wrap (GTK_LABEL (helptext), TRUE);
-	gtk_container_add (GTK_CONTAINER (align1), helptext);
-	gtk_container_set_border_width (GTK_CONTAINER (align1), 6);
-	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), align1, helplabel);
+	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), helptext, helplabel);
 	
 	
 	gtk_label_set_markup (GTK_LABEL (keytext),
@@ -102,20 +92,28 @@ static GtkWidget *create_help (gint page)
 "<b>CTRL+o</b>: Copies the contents of a file into a pad.\n\n"
 "<b>CTRL+p</b>: Opens the preferences window.\n\n"
 "<b>CTRL+q</b>: Quits xpad.\n\n"
-"<b>CTRL+SHIFT+d</b>: Deletes the currently selected pad.\n");
+"<b>CTRL+SHIFT+d</b>: Deletes the currently selected pad.\n\n"
+"<b>CTRL+Left drag</b>: Moves the pad.\n\n"
+"<b>CTRL+Right drag</b>: Resizes the pad.\n\n");
 	
+	gtk_misc_set_padding (GTK_MISC (keytext), 12, 12);
+	gtk_misc_set_alignment (GTK_MISC (keytext), 0, 0);
 	gtk_label_set_line_wrap (GTK_LABEL (keytext), TRUE);
-	gtk_container_add (GTK_CONTAINER (align2), keytext);
-	gtk_container_set_border_width (GTK_CONTAINER (align2), 6);
-	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), align2, keylabel);
+	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), keytext, keylabel);
 	
 	
-	gtk_label_set_markup (GTK_LABEL (edittext), longtext); /* see above */
+	gtk_label_set_markup (GTK_LABEL (styletext),
+"If a pad's style is locked, any changes made in the preference "
+"window to the text, background, or border color or the font face "
+"do not affect that pad.\n\n"
+"To lock a pad's style, right click on a "
+"pad to open the popup menu and select \"Lock Style\".  "
+"By default, pads are not locked.");
 
-	gtk_label_set_line_wrap (GTK_LABEL (edittext), TRUE);
-	gtk_container_add (GTK_CONTAINER (align3), edittext);
-	gtk_container_set_border_width (GTK_CONTAINER (align3), 6);
-	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), align3, editlabel);
+	gtk_misc_set_padding (GTK_MISC (styletext), 12, 12);
+	gtk_misc_set_alignment (GTK_MISC (styletext), 0, 0);
+	gtk_label_set_line_wrap (GTK_LABEL (styletext), TRUE);
+	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), styletext, stylelabel);
 	
 	gtk_notebook_set_current_page (GTK_NOTEBOOK (notebook), page);
 	
