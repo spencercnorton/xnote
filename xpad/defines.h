6dcbe5c2c7a3b4fb0562f8438399cf827cc7a1e9
mikix <>
2002-08-21 03:54:35 +0000

    v1.5 documentation changes and version bumping

--- xpad/defines.h
+++ xpad/defines.h
@@ -10,7 +10,7 @@
 /* files we use are not large */
 #define MAX_FILE_SIZE		1024
 
-#define VERSION				"1.4"
+#define VERSION				"1.5"
 
 /* now define some structures here so that i don't have odd header dependencies */
 typedef struct xpad_toolbar_def xpad_toolbar;
