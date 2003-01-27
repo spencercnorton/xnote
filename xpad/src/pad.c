299d0802436794edcfacf4c6e578e664b2789654
mikix <>
2003-01-27 05:35:21 +0000

    use some gtk2.2 api calls to make windows nicer

--- xpad/src/pad.c
+++ xpad/src/pad.c
@@ -1905,6 +1905,11 @@ pad_alloc_gtk (pad_node *pad)
 	g_signal_connect_after (textbox, "realize", G_CALLBACK 
 		(pad_when_textbox_realized), pad);
 	
+#if ((GTK_MAJOR_VERSION == 2) && (GTK_MINOR_VERSION == 2))
+	gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_UTILITY);
+	gtk_window_set_skip_taskbar_hint (GTK_WINDOW (window), TRUE);
+#endif
+	
 	g_signal_connect (gtk_scrolled_window_get_vadjustment (GTK_SCROLLED_WINDOW (pad->scrollbar))
 		, "value-changed", G_CALLBACK (pad_scrolled), pad);
 	g_signal_connect (gtk_scrolled_window_get_hadjustment (GTK_SCROLLED_WINDOW (pad->scrollbar))
