b2df6e1f1407a9ad5404e2d1ee5b527be5f72f74
mikix <>
2003-03-17 07:33:55 +0000

    move set_type_hint outside of 2.2 ifdef

--- xpad/src/pad.c
+++ xpad/src/pad.c
@@ -1895,8 +1895,9 @@ pad_alloc_gtk (pad_node *pad, const gchar *role)
 	g_signal_connect_after (textbox, "realize", G_CALLBACK 
 		(pad_when_textbox_realized), pad);
 	
-#if ((GTK_MAJOR_VERSION == 2) && (GTK_MINOR_VERSION >= 2))
 	gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_UTILITY);
+	
+#if ((GTK_MAJOR_VERSION == 2) && (GTK_MINOR_VERSION >= 2))
 	gtk_window_set_skip_taskbar_hint (GTK_WINDOW (window), TRUE);
 #endif
 	
