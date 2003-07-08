4deda44f8387e2f7f112edb630e596119ace603e
mikix <>
2003-07-08 01:08:50 +0000

    use autoreconf

--- xpad/autogen.sh
+++ xpad/autogen.sh
@@ -1,13 +1,2 @@
 #!/bin/sh
-echo '* running gettextize...'
-gettextize --force --intl --no-changelog
-echo '* running aclocal...'
-aclocal -I m4
-echo '* running autoheader...'
-autoheader --force
-echo '* running automake...'
-automake --foreign --add-missing --force-missing
-echo '* running autoconf...'
-autoconf --force
-echo '* running configure...'
-./configure && echo "* configure succeeded. Type 'make' to build."
+autoreconf --force --install --symlink
