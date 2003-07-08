59513bd449c6cfdf62d48112be5c6cda6a76c09a
mikix <>
2003-07-08 00:19:26 +0000

    getting rid of autogenerated files

--- xpad/autogen.sh
+++ xpad/autogen.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
-touch stamp-h
+echo '* running gettextize...'
+gettextize --force --intl --no-changelog
 echo '* running aclocal...'
-aclocal
-#echo '* running libtoolize...'
-#libtoolize --force
-#echo '* linking config/ltmain.sh to ./ltmain.sh...'
-#cd config && ln -sf ../ltmain.sh && cd ..
+aclocal -I m4
 echo '* running autoheader...'
 autoheader --force
 echo '* running automake...'
 automake --foreign --add-missing --force-missing
-echo '* running gettextize...'
-gettextize --force --intl --no-changelog
-echo '* re-running aclocal...'
-aclocal -I m4
 echo '* running autoconf...'
 autoconf --force
 echo '* running configure...'
