e2c99c7fb19fedfea91207487d6a072bc6adcc30
mikix <>
2005-01-14 03:42:10 +0000

    *** empty log message ***

--- xpad/src/fio.c
+++ xpad/src/fio.c
@@ -29,11 +29,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "fio.h"
 #include "xpad-app.h"
 
-
-/* A cache of whether we can use the .xpad directory */
-static gboolean can_write = TRUE;
-
-
 /* sets filename to full path of filename (prepends xpad_app_get_config_dir () to it) 
    returns 0 if filename was full path, 1 if we added to it.
    
