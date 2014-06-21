a3844ce323cf2f589d439b76aceb5404b5c7a051
Arthur Borsboom <arthurborsboom@gmail.com>
2014-06-21 13:23:37 +0200

    * Fix: prevented a compiler warning * Fix: removed spaces

--- src/xpad-session-manager.c
+++ src/xpad-session-manager.c
@@ -61,25 +61,25 @@ xpad_session_manager_cycle (GIOChannel *source, GIOCondition condition,
 
 	gboolean rv = TRUE;
 	IceConn ice_conn;
-	
+
 	ice_conn = SmcGetIceConnection (xpad_session_manager_conn);
-	
+
 	switch (IceProcessMessages (ice_conn, NULL, NULL)) {
 	default:
 	case IceProcessMessagesSuccess:
 		break;
-	
+
 	case IceProcessMessagesIOError:
 		xpad_session_manager_shutdown ();
 		rv = FALSE;
 		break;
-	
+
 	case IceProcessMessagesConnectionClosed:
 		xpad_session_manager_conn = NULL;
 		rv = FALSE;
 		break;
 	}
-	
+
 	return rv;
 }
 
@@ -87,17 +87,17 @@ static gboolean
 xpad_session_manager_add_cycle_to_main_loop ()
 {
   GIOChannel *channel;
-  
+
 	if (!xpad_session_manager_conn)
 		return FALSE;
-	
+
 	if (xpad_ice_fd == -1)
 	  return FALSE;
-	
+
 	channel = g_io_channel_unix_new (xpad_ice_fd);
 	g_io_add_watch (channel, G_IO_IN, xpad_session_manager_cycle, NULL);
 	g_io_channel_unref (channel);
-	
+
 	return FALSE;
 }
 
@@ -117,39 +117,39 @@ gboolean
 xpad_session_manager_start_interact (gboolean error)
 {
 	Status status;
-	
+
 	if (!xpad_session_manager_conn)
 		return TRUE;
-	
+
 	if (!xpad_saving)
 		return TRUE;
-	
+
 	switch (xpad_interact_style) {
 	case SmInteractStyleNone:
 		return FALSE;
-	
+
 	case SmInteractStyleErrors:
 		if (!error)
 			return FALSE;
 		break;
-	
+
 	case SmInteractStyleAny:
 		break;
-	
+
 	default:
 		return TRUE;
 	}
-	
+
 	blocking = TRUE;
-	status = SmcInteractRequest (xpad_session_manager_conn, 
+	status = SmcInteractRequest (xpad_session_manager_conn,
 		error ? SmDialogError : SmDialogNormal,
 		xpad_session_manager_start_interact_callback, NULL);
-	
+
 	if (status)
 		gtk_main ();
 	else
 		blocking = FALSE;
-	
+
 	return status ? TRUE : FALSE;
 }
 
@@ -158,10 +158,10 @@ xpad_session_manager_stop_interact (gboolean stop_shutdown)
 {
 	if (!xpad_session_manager_conn)
 		return;
-	
+
 	if (!blocking)
 		return;
-	
+
 	SmcInteractDone (xpad_session_manager_conn, stop_shutdown);
 }
 
@@ -174,12 +174,12 @@ xpad_session_manager_ice_connection_watch (IceConn ice_conn,
 	(void) watch_data;
 
 	int fd = IceConnectionNumber (ice_conn);
-	
+
 	if (opening)
 		xpad_ice_fd = fd;
 	else
 		xpad_ice_fd = -1;
-  
+
 	xpad_session_manager_add_cycle_to_main_loop ();
 }
 
@@ -206,16 +206,16 @@ xpad_session_manager_set_properties (void)
 	gchar *pid_str;
 	gchar *command = g_strdup (xpad_app_get_program_path ());
 	size_t string_length = 0;
-	
+
 	prop[0].vals = vals.clone;
 	prop[1].vals = vals.program;
 	prop[2].vals = vals.restart;
 	prop[3].vals = vals.user;
 	prop[4].vals = vals.process;
-	
+
 	for (i = 0; i < G_N_ELEMENTS (prop); i++)
 		props[i] = &prop[i];
-	
+
 	pw = getpwuid (getuid ());
 
 	/* While setting all the properties, safe casts are being used. */
@@ -225,53 +225,53 @@ xpad_session_manager_set_properties (void)
 	if (string_length <= INT_MAX)
 		vals.user->length = (int) string_length;
 	else
-		g_warning(casting_warning);
-	
+		g_warning("%s", casting_warning);
+
 	vals.program->value = command;
 	string_length = strlen (vals.program->value);
 	if (string_length <= INT_MAX)
 		vals.program->length = (int) string_length;
 	else
-		g_warning(casting_warning);
-	
+		g_warning("%s", casting_warning);
+
 	vals.clone->value = command;
 	string_length = strlen (vals.clone->value);
 	if (string_length <= INT_MAX)
 		vals.clone->length = (int) string_length;
 	else
-		g_warning(casting_warning);
-	
+		g_warning("%s", casting_warning);
+
 	vals.restart[0].value = command;
 	string_length = strlen (vals.restart[0].value);
 	if (string_length <= INT_MAX)
 		vals.restart[0].length = (int) string_length;
 	else
-		g_warning(casting_warning);
+		g_warning("%s", casting_warning);
 
 	vals.restart[1].value = "--sm-client-id";
 	string_length = strlen (vals.restart[1].value);
 	if (string_length <= INT_MAX)
 		vals.restart[1].length = (int) string_length;
 	else
-		g_warning(casting_warning);
+		g_warning("%s", casting_warning);
 
 	vals.restart[2].value = client_id;
 	string_length = strlen (vals.restart[2].value);
 	if (string_length <= INT_MAX)
 		vals.restart[2].length = (int) string_length;
 	else
-		g_warning(casting_warning);
-	
+		g_warning("%s", casting_warning);
+
 	pid_str = g_strdup_printf ("%i", getpid ());
 	vals.process->value = pid_str;
 	string_length = strlen (vals.process->value);
 	if (string_length <= INT_MAX)
 		vals.process->length = (int) string_length;
 	else
-		g_warning(casting_warning);
-	
+		g_warning("%s", casting_warning);
+
 	SmcSetProperties (xpad_session_manager_conn, 4, (SmProp **) &props);
-	
+
 	g_free (pid_str);
 	g_free (command);
 }
@@ -291,25 +291,25 @@ xpad_session_manager_init (void)
 		{xpad_session_manager_die, NULL},
 		{xpad_session_manager_save_complete, NULL},
 		{xpad_session_manager_shutdown_cancelled, NULL}};
-	
+
 	if (xpad_session_manager_conn) {
 		/* we were already connected... */
 		xpad_session_manager_shutdown ();
 	}
-	
+
 	xpad_saving = FALSE;
 	xpad_interact_style = SmInteractStyleAny;
 	xpad_session_manager_conn = SmcOpenConnection (NULL, NULL, 
 		SmProtoMajor, SmProtoMinor,
 		mask, &callbacks, client_id, &client_id_ret,
 		100, error_string_net);
-	
+
 	client_id = g_strdup (client_id_ret);
 	free (client_id_ret);
-	
+
 	xpad_ice_fd = -1;
 	IceAddConnectionWatch (xpad_session_manager_ice_connection_watch, NULL);
-	
+
 	gdk_x11_set_sm_client_id (client_id);
 }
 
@@ -317,12 +317,12 @@ void
 xpad_session_manager_set_id (const gchar *id)
 {
 	gboolean alread_set_up = xpad_session_manager_conn ? TRUE : FALSE;
-	
+
 	if (alread_set_up)
 		xpad_session_manager_shutdown ();
-	
+
 	client_id = g_strdup (id);
-	
+
 	if (alread_set_up)
 		xpad_session_manager_init ();
 }
@@ -332,13 +332,13 @@ xpad_session_manager_shutdown (void)
 {
 	if (!xpad_session_manager_conn)
 		return;
-	
+
 	if (client_id)
 	{
 		g_free (client_id);
 		client_id = NULL;
 	}
-	
+
 	SmcCloseConnection (xpad_session_manager_conn, 0, NULL);
 	xpad_session_manager_conn = NULL;
 }
@@ -366,26 +366,26 @@ xpad_session_manager_save_yourself (SmcConn smc_conn, SmPointer client_data,
 	(void) client_data;
 
 	RETURN_IF_BAD_CONN (smc_conn);
-	
+
 	xpad_interact_style = interact_style;
 	xpad_shutdown = shutdown ? TRUE : FALSE;
 	xpad_saving = TRUE;
-	
+
 	switch (save_type) {
 	case SmSaveLocal:
 		xpad_session_manager_save_local (fast);
 		break;
-	
+
 	case SmSaveGlobal:
 		break;
-	
+
 	case SmSaveBoth:
 		xpad_session_manager_save_local (fast);
 		break;
 	}
-	
+
 	SmcSaveYourselfDone (smc_conn, True);
-	
+
 	if (shutdown)
 		xpad_interact_style = SmInteractStyleNone;
 }
@@ -398,13 +398,13 @@ xpad_session_manager_die (SmcConn smc_conn, SmPointer client_data)
 
 	RETURN_IF_BAD_CONN (smc_conn);
 	xpad_shutdown = True;
-	
+
 	if (blocking)
 	  gtk_main_quit ();
 	blocking = FALSE;
 	xpad_saving = FALSE;
 	xpad_interact_style = SmInteractStyleAny;
-	
+
 	gtk_main_quit ();
 }
 
@@ -417,7 +417,7 @@ xpad_session_manager_shutdown_cancelled (SmcConn smc_conn, SmPointer client_data
 	RETURN_IF_BAD_CONN (smc_conn);
 	RETURN_IF_NOT_SAVING ();
 	xpad_shutdown = False;
-	
+
 	if (blocking)
 	  gtk_main_quit ();
 	blocking = FALSE;
@@ -433,7 +433,7 @@ xpad_session_manager_save_complete (SmcConn smc_conn, SmPointer client_data)
 
 	RETURN_IF_BAD_CONN (smc_conn);
 	RETURN_IF_NOT_SAVING ();
-	
+
 	if (blocking)
 	  gtk_main_quit ();
 	blocking = FALSE;
@@ -441,7 +441,7 @@ xpad_session_manager_save_complete (SmcConn smc_conn, SmPointer client_data)
 	xpad_interact_style = SmInteractStyleAny;
 }
 
-#else 
+#else
 
 gboolean
 xpad_session_manager_start_interact (gboolean error)
