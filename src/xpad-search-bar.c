fb264e270de739155ef9997d40ad15ea2db3a422
Arthur Borsboom <arthurborsboom@gmail.com>
2021-05-11 17:44:47 +0200

    Fix: C++ style comments are not allowed in ISO C90

--- src/xpad-search-bar.c
+++ src/xpad-search-bar.c
@@ -112,28 +112,30 @@ xpad_search_bar_constructed (GObject *object)
 {
 	XpadSearchBar *searchbar = XPAD_SEARCH_BAR (object);
 
-	// Horizontal box with search_entry and up/down buttons
+	/* Horizontal box with search_entry and up/down buttons */
 	GtkBox *hbox = GTK_BOX (gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0));
 	gtk_box_set_homogeneous (hbox, FALSE);
 	gtk_container_add (GTK_CONTAINER (searchbar), GTK_WIDGET (hbox));
 
-	// search_entry
+	/* search_entry */
 	searchbar->priv->search_entry = GTK_SEARCH_ENTRY (gtk_search_entry_new ());
 	gtk_box_pack_start (hbox, GTK_WIDGET (searchbar->priv->search_entry), TRUE, TRUE, 0);
 
-	// previous match button
+	/* previous match button */
 	GtkButton *prev_search_btn = GTK_BUTTON (gtk_button_new_from_icon_name ("go-up-symbolic", GTK_ICON_SIZE_SMALL_TOOLBAR));
 	gtk_box_pack_start (hbox, GTK_WIDGET (prev_search_btn), FALSE, FALSE, 0);
 
-	// next match button
+	/* next match button */
 	GtkButton *forward_search_btn = GTK_BUTTON (gtk_button_new_from_icon_name ("go-down-symbolic", GTK_ICON_SIZE_SMALL_TOOLBAR));
 	gtk_box_pack_start (hbox, GTK_WIDGET (forward_search_btn), FALSE, FALSE, 0);
 
-	// this will allow to for example press Esc without moving the mouse exactly
-	// Sinto the search field to remove the search bar
+	/*
+	 * this will allow to for example press Esc without moving the mouse exactly
+	 * Sinto the search field to remove the search bar
+	 */
 	gtk_search_bar_connect_entry (GTK_SEARCH_BAR (searchbar), GTK_ENTRY (searchbar->priv->search_entry));
 
-	// connecting to GtkSearchEntry signals for actual searching
+	/* connecting to GtkSearchEntry signals for actual searching */
 	g_signal_connect_swapped (searchbar->priv->search_entry, "stop-search", G_CALLBACK (xpad_search_bar_stop_search_cb), searchbar);
 	g_signal_connect_swapped (searchbar->priv->search_entry, "search-changed", G_CALLBACK (xpad_search_bar_search_changed_cb), searchbar);
 	g_signal_connect_swapped (searchbar->priv->search_entry, "previous-match", G_CALLBACK (xpad_search_bar_previous_match_cb), searchbar);
@@ -142,37 +144,41 @@ xpad_search_bar_constructed (GObject *object)
 	g_signal_connect_swapped (prev_search_btn, "clicked", G_CALLBACK (xpad_search_bar_previous_match_cb), searchbar);
 	g_signal_connect_swapped (forward_search_btn, "clicked", G_CALLBACK (xpad_search_bar_next_match_cb), searchbar);
 
-	// also allow to close by a button on a searchbar (which is implemented in GtkSearchBar itself)
-	// (in gedit search entry dissappears when cursor is moved to text buffer, choosing not to do
-	// the same since it is actually irritating, in case you want to search something, change something,
-	// search again for the same thing. Not adding search and replace since xpad is not a full-blown
-	// text editor, but not hiding search entry when cursor is moved to text buffer is at least a
-	// step in this direction
+	/*
+	 * also allow to close by a button on a searchbar (which is implemented in GtkSearchBar itself)
+	 * (in gedit search entry dissappears when cursor is moved to text buffer, choosing not to do
+	 * the same since it is actually irritating, in case you want to search something, change something,
+	 * search again for the same thing. Not adding search and replace since xpad is not a full-blown
+	 * text editor, but not hiding search entry when cursor is moved to text buffer is at least a
+	 * step in this direction
+	 */
 	gtk_search_bar_set_show_close_button (GTK_SEARCH_BAR(searchbar), TRUE);
 
-	// Searchbar should appear in right-top corner of the overlay with text
-	// to not cover the text which is searched (assuming that first
-	// several lines most probably are short enough to not be covered)
+	/* Searchbar should appear in right-top corner of the overlay with text
+	 * to not cover the text which is searched (assuming that first
+	 * several lines most probably are short enough to not be covered)
+	 */
 	gtk_widget_set_halign (GTK_WIDGET (searchbar), GTK_ALIGN_END);
 	gtk_widget_set_valign (GTK_WIDGET (searchbar), GTK_ALIGN_START);
 
-	// Create GtkSearchContext with source buffer as parameter, this will be used for the actual search
+	/* Create GtkSearchContext with source buffer as parameter, this will be used for the actual search */
 	searchbar->priv->search_context = gtk_source_search_context_new (searchbar->priv->source_buffer, NULL);
 
 	GtkSourceSearchSettings *settings = gtk_source_search_context_get_settings (searchbar->priv->search_context);
 
-	// Setting wrap-around to true to ease life. This means that next/prev buttons in searchbar
-	// don't need to be greyed out in case we've reached end of document with forward search
-	// (or beginnig with backward search), they will just wrap arround. If pressing enter
-	// in search entry (which is set to forward search) or pressing next/prev buttons in searchbar
-	// will not find anything we then highlight search entry by red background. This is
-	// similar to gedit where search entry is highlighted in the same way but not after
-	// enter/next/prev are pressed but with complicated logic to do it on the fly (as buffer is
-	// scanned asynchronously there may be a situation when gtk_source_search_context_get_occurrences_count
-	// does not return actual count since it is not counted yet and then they wait for several amount
-	// of time to retry). To avoid this complex logic, wrap-around is set to true to never grey out next/prev
-	// buttons and search entry is highlighted red only if enter/next/prev was pressed and nothing was found
-	// (any change inside search entry will remove this highlight, same for initiating another search)
+	/* Setting wrap-around to true to ease life. This means that next/prev buttons in searchbar
+	 * don't need to be greyed out in case we've reached end of document with forward search
+	 * (or beginnig with backward search), they will just wrap arround. If pressing enter
+	 * in search entry (which is set to forward search) or pressing next/prev buttons in searchbar
+	 * will not find anything we then highlight search entry by red background. This is
+	 * similar to gedit where search entry is highlighted in the same way but not after
+	 * enter/next/prev are pressed but with complicated logic to do it on the fly (as buffer is
+	 * scanned asynchronously there may be a situation when gtk_source_search_context_get_occurrences_count
+	 * does not return actual count since it is not counted yet and then they wait for several amount
+	 * of time to retry). To avoid this complex logic, wrap-around is set to true to never grey out next/prev
+	 * buttons and search entry is highlighted red only if enter/next/prev was pressed and nothing was found
+	 * (any change inside search entry will remove this highlight, same for initiating another search)
+	 */
 	gtk_source_search_settings_set_wrap_around (settings, TRUE);
 }
 
@@ -235,16 +241,19 @@ xpad_search_bar_finalize (GObject *object)
 static void
 xpad_search_bar_previous_match_cb(XpadSearchBar *searchbar)
 {
-	// reset error highlighting because the text which was not found
-	// might have just appeared (user might have enterd it)
+	/*
+	 * reset error highlighting because the text which was not found
+	 * might have just appeared (user might have enterd it)
+	 */
 	set_not_found_state (searchbar, FALSE);
 
 	GtkTextBuffer *buffer = GTK_TEXT_BUFFER (searchbar->priv->source_buffer);
 
-	// See comment in xpad_search_bar_next_match_cb, this is exactly the same
-	// but as we are searching backwards this time we look for the beginning
-	// of selection (or in case there is no selection - for the position of cursor
-	// which is equal to both beginning and end of selection)
+	/* See comment in xpad_search_bar_next_match_cb, this is exactly the same
+	 * but as we are searching backwards this time we look for the beginning
+	 * of selection (or in case there is no selection - for the position of cursor
+	 * which is equal to both beginning and end of selection)
+	 */
 	GtkTextIter start;
 	gtk_text_buffer_get_selection_bounds (buffer, &start, NULL);
 
@@ -268,18 +277,20 @@ xpad_search_bar_previous_match_cb(XpadSearchBar *searchbar)
 static void
 xpad_search_bar_next_match_cb(XpadSearchBar *searchbar)
 {
-	// reset error highlighting because the text which was not found
-	// might have just appeared (user might have enterd it)
+	/* reset error highlighting because the text which was not found
+	 * might have just appeared (user might have enterd it)
+	 */
 	set_not_found_state (searchbar, FALSE);
 
 	GtkTextBuffer *buffer = GTK_TEXT_BUFFER (searchbar->priv->source_buffer);
 
-	// In case something was selected by ongoing search (pressing next -> next -> next
-	// start will point to the end of selection and next search will start from there,
-	// otherwise (user has moved cursor away from search entry into the text buffer),
-	// start will then point to the exact location of the cursor and next search will
-	// begin from there. This allows to handle situation when user changes text, goes
-	// back to search entry, searches several times, again changes text and so on
+	/* In case something was selected by ongoing search (pressing next -> next -> next
+	 * start will point to the end of selection and next search will start from there,
+	 * otherwise (user has moved cursor away from search entry into the text buffer),
+	 * start will then point to the exact location of the cursor and next search will
+	 * begin from there. This allows to handle situation when user changes text, goes
+	 * back to search entry, searches several times, again changes text and so on
+	 */
 	GtkTextIter start;
 	gtk_text_buffer_get_selection_bounds (buffer, NULL, &start);
 
