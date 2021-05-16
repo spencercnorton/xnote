b608f39c9632e669ebe8f43f5c6a67ea2a7938ec
Arthur Borsboom <arthurborsboom@gmail.com>
2021-05-16 13:11:12 +0200

    Add libappindicator as dependency to the build system

--- src/xpad-tray.c
+++ src/xpad-tray.c
@@ -25,6 +25,10 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
 
+#ifdef HAVE_APP_INDICATOR
+#include <libappindicator/app-indicator.h>
+#endif /* HAVE_APP_INDICATOR */
+
 #include "xpad-tray.h"
 #include "xpad-app.h"
 #include "xpad-pad.h"
